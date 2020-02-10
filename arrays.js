85779dcc3aa7bac041619183243eecc072187672
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-10 18:25:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitcat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  [...array, element]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element)
+}
